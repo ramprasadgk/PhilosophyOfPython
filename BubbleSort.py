fd34f064309df225a3bdb52e82bee6cba1c96c51
ramprasad <ramapkri@oradev.oraclecorp.com>
2019-03-12 20:51:23 +0530

    bsort

---
+++ BubbleSort.py
@@ -0,0 +1,23 @@
+print ('begin')
+
+def bubblesort(U):
+    swapped = False
+    for i in range(len(U)):
+        swapped = False
+        for j in range(len(U)-1-i):
+            if(U[j] > U[j+1]):
+                U[j],U[j+1]= U[j+1],U[j]
+                print ("swapped ",U[j], 'and',U[j+1])
+                swapped = True
+                print (U)
+            else:
+                print ("No need to swap ",U[j], 'and',U[j+1])
+                print(U)
+        if not swapped:
+            return
+    
+
+if __name__ == '__main__':
+    unsorted = [4,1,3,2,5]
+    bubblesort(unsorted)
+    
